9e18ca059d3aa23fe2aa3d38ebbceb624b5e48f9
TungNguyen248 <136042948+rdagol@users.noreply.github.com>
2026-06-22 22:10:47 +0700

    Simple Navigate
    Tạo 2 button chia 2 màn hình khác nhau

--- lib/main.dart
+++ lib/main.dart
@@ -31,16 +31,58 @@ class HomeScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             ElevatedButton(
-              onPressed: () {},
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const ToeicScreen()),
+                );
+              },
               child: const Text('TOEIC'),
             ),
             const SizedBox(height: 20),
             ElevatedButton(
-              onPressed: () {},
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const IeltsScreen()),
+                );
+              },
               child: const Text('IELTS'),
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class ToeicScreen extends StatelessWidget {
+  const ToeicScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('TOEIC Practice'),
+      ),
+      body: const Center(
+        child: Text('TOEIC Screen'),
+      ),
+    );
+  }
+}
+
+class IeltsScreen extends StatelessWidget {
+  const IeltsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('IELTS Practice'),
+      ),
+      body: const Center(
+        child: Text('IELTS Screen'),
       ),
     );
   }
